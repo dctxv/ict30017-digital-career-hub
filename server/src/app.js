cf961c1b84891a4268c88bfc83c5fe2e66a3bcef
unknown <dwijeshu003@gmail.com>
2026-05-26 01:18:57 +1000

    Isar: Frontend and data APIs - disciplines, career paths, resources, alumni, admin dashboard, working resource links

--- server/src/app.js
+++ server/src/app.js
@@ -40,12 +40,25 @@ app.get('/api/test-db', async (req, res) => {
 
 // Routes
 import resumeRouter from './routes/resume.js';
+<<<<<<< Updated upstream
 import authRouter from './routes/auth.js';
 app.use('/api/resume', resumeRouter);
 app.use('/api/auth', authRouter);
 import chatbotRouter from './routes/chatbot.js';
 app.use('/api/resume', resumeRouter);
 app.use('/api/chat', chatbotRouter);
+=======
+import disciplineRoutes from './routes/disciplines.js';
+import careerPathsRoutes from './routes/careerPaths.js';
+import resourcesRoutes from './routes/resources.js';
+import alumniRoutes from './routes/alumni.js';
+app.use('/api/alumni', alumniRoutes);
+
+app.use('/api/resume', resumeRouter);
+app.use('/api/disciplines', disciplineRoutes);
+app.use('/api/career-paths', careerPathsRoutes);
+app.use('/api/resources', resourcesRoutes);
+>>>>>>> Stashed changes
 
 // Error handler
 app.use((err, req, res, next) => {
